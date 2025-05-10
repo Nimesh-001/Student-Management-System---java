d3e8f0b772a4ae835fb7ffafc16491160fce0bd4
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-10 13:16:29 +0530

    Update Admindashbord.java

--- Student Management System/src/Admindashbord.java
+++ Student Management System/src/Admindashbord.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Admindashbord {
     private JPanel panel1;
@@ -10,4 +12,61 @@ public class Admindashbord {
     private JButton noticesButton1;
     private JButton coursesButton1;
     private JButton userProfilesButton1;
+
+    public Admindashbord() {
+
+        JFrame frame = new JFrame("Admindashbord");
+        frame.setContentPane(this.panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+
+        userProfilesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        coursesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        noticesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        timetablesButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        editProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new Login();
+
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        new Admindashbord();
+
+    }
 }

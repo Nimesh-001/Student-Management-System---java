5fc7f01d350fd7a492fc4ef4fc060b20c72f8055
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-10 09:55:10 +0530

    Update Demo.java

--- Student Management System/src/Demo.java
+++ Student Management System/src/Demo.java
@@ -2,8 +2,8 @@ import java.sql.Connection;
 
 public class Demo {
     public static void main(String[] args) {
-        Dbconnector dc = new Dbconnector();
-        Connection conn = dc.getConnection();
+        Dbconnector dbc = new Dbconnector();
+        Connection conn = dbc.getConnection();
 
         if (conn != null) {
             System.out.println("Database connection established");

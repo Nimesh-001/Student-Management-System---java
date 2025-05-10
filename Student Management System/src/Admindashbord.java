da1855b477fd27680cbdb4147cf37280db7aa1fe
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2025-05-10 13:09:41 +0530

    update

--- Student Management System/src/Admindashbord.java
+++ Student Management System/src/Admindashbord.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class Admindashbord {
+    private JPanel panel1;
+    private JLabel profilePicLabel;
+    private JButton logoutButton;
+    private JButton editProfileButton;
+    private JLabel adminUsernameLabel;
+    private JButton timetablesButton1;
+    private JButton noticesButton1;
+    private JButton coursesButton1;
+    private JButton userProfilesButton1;
 }
